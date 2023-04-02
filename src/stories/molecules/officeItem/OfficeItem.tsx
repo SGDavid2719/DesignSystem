b3c0b93dfcace958c1a3f4a61b8f40f9d8ee4a06
David Santomé <santome.galvan.david@gmail.com>
2023-04-02 20:22:25 +0200

    [Fix] Don't use span as container

--- src/stories/molecules/officeItem/OfficeItem.tsx
+++ src/stories/molecules/officeItem/OfficeItem.tsx
@@ -35,68 +35,46 @@ export const OfficeItem = ({
         <div className="mb-4">
             <div className="text-xl mb-4">{name}</div>
             <div className="flex">
-                <Span
-                    children={
-                        <>
-                            {" "}
-                            <Icon
-                                iconId="location"
-                                iconClassName="inline-block text-blue-400"
-                            />{" "}
-                            {location}{" "}
-                        </>
-                    }
-                />
+                <div>
+                    <Icon
+                        iconId="location"
+                        iconClassName="inline-block text-blue-400"
+                    />
+                    <Span spanClassName="ml-1" children={location} />
+                </div>
             </div>
             <div>
-                <Span
-                    children={
-                        <>
-                            {" "}
-                            <Icon
-                                iconId="phone"
-                                iconClassName="inline-block text-blue-400"
-                            />{" "}
-                            {phoneNumber}{" "}
-                        </>
-                    }
-                />
+                <div>
+                    <Icon
+                        iconId="phone"
+                        iconClassName="inline-block text-blue-400"
+                    />
+                    <Span spanClassName="ml-1" children={phoneNumber} />
+                </div>
             </div>
             <div>
-                <Span
-                    children={
-                        <>
-                            {" "}
-                            <Icon
-                                iconId="mail"
-                                iconClassName="inline-block text-blue-400"
-                            />{" "}
-                            {email}{" "}
-                        </>
-                    }
-                />
+                <div>
+                    <Icon
+                        iconId="mail"
+                        iconClassName="inline-block text-blue-400"
+                    />
+                    <Span spanClassName="ml-1" children={email} />
+                </div>
             </div>
             <div>
-                <Span
-                    children={
-                        <>
-                            {" "}
-                            <Icon
-                                iconId="schedule"
-                                iconClassName="inline-block text-blue-400"
-                            />{" "}
-                            {schedules.map((schedule, index) => (
-                                <Span
-                                    key={index}
-                                    children={schedule}
-                                    spanClassName={`${
-                                        index !== 0 ? "block" : ""
-                                    }`}
-                                />
-                            ))}{" "}
-                        </>
-                    }
-                />
+                <div>
+                    <Icon
+                        iconId="schedule"
+                        iconClassName="inline-block text-blue-400"
+                    />
+                    {schedules.map((schedule, index) => (
+                        <Span
+                            key={index}
+                            children={schedule}
+                            spanClassName={`${index !== 0 ? "block" : "ml-1"}`}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
     );
